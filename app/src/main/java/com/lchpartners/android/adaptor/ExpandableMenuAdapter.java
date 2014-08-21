d2bb51fef3e24dddf95c8a6bb4f47ae492af7db0
kgwangrae <k.gwangrae@gmail.com>
2014-08-22 02:07:22 +0900

    Deleted generated files in .idea from git, Did some small refactoring (name changes ..) to ExpandableMenuAdapter

--- app/src/main/java/com/lchpartners/android/adaptor/ExpandableMenuAdapter.java
+++ app/src/main/java/com/lchpartners/android/adaptor/ExpandableMenuAdapter.java
@@ -1,9 +1,5 @@
 package com.lchpartners.android.adaptor;
 
-import java.util.ArrayList;
-
-import com.lchpartners.shadal.R;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +7,22 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import com.lchpartners.shadal.R;
+
+import java.util.ArrayList;
+
 public class ExpandableMenuAdapter extends BaseExpandableListAdapter {
 
+    //Data references
 	private ArrayList<String> catList;
 	private ArrayList<ArrayList<String>> menuList;
 	private ArrayList<ArrayList<String>> priceList;
 	private LayoutInflater inflater;
-	
+
+    private static class MenuViewHolder {
+        public TextView menuText;
+        public TextView priceText;
+    }
 	
 	public ExpandableMenuAdapter(Context c, ArrayList<String> catList,
 			ArrayList<ArrayList<String>> menuList,
@@ -30,79 +35,80 @@ public class ExpandableMenuAdapter extends BaseExpandableListAdapter {
 	}
 
 	@Override
-	public Object getChild(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-		return menuList.get(arg0).get(arg1);
+	public Object getChild(int groupPosition, int childPosition) {
+		return menuList.get(groupPosition).get(childPosition);
 	}
 
 	@Override
-	public long getChildId(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
 	}
 
 	@Override
-	public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-			ViewGroup arg4) {
-		// TODO Auto-generated method stub
-		View v = arg3;
-		if(v == null)
-			v = inflater.inflate(R.layout.expandable_item, null);
-		
-		TextView menuText = (TextView)v.findViewById(R.id.textView_time);
-		TextView priceText = (TextView)v.findViewById(R.id.textView_menuprice);
-		menuText.setText(menuList.get(arg0).get(arg1));
-		priceText.setText(priceList.get(arg0).get(arg1));
+	public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
+                             View convertView, ViewGroup parent) {
+		View resultView = convertView;
+        TextView menuText, priceText;
+		if(resultView == null) {
+            resultView = inflater.inflate(R.layout.expandable_item, null);
+            MenuViewHolder menuViewHolder = new MenuViewHolder();
 
-		return v;
+            menuText = (TextView)resultView.findViewById(R.id.textView_time);
+            priceText = (TextView)resultView.findViewById(R.id.textView_menuprice);
+            menuViewHolder.menuText = menuText;
+            menuViewHolder.priceText = priceText;
+            resultView.setTag(menuViewHolder);
+        }
+        else {
+            MenuViewHolder menuViewHolder = (MenuViewHolder) resultView.getTag();
+            menuText = menuViewHolder.menuText;
+            priceText = menuViewHolder.priceText;
+        }
+		menuText.setText(menuList.get(groupPosition).get(childPosition));
+		priceText.setText(priceList.get(groupPosition).get(childPosition));
+
+		return resultView;
 	}
 
 	@Override
-	public int getChildrenCount(int arg0) {
-		// TODO Auto-generated method stub
-		return menuList.get(arg0).size();
+	public int getChildrenCount(int groupPosition) {
+		return menuList.get(groupPosition).size();
 	}
 
 	@Override
-	public Object getGroup(int arg0) {
-		// TODO Auto-generated method stub
-		return menuList.get(arg0);
+	public Object getGroup(int groupPosition) {
+		return menuList.get(groupPosition);
 	}
 
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
 		return catList.size();
 	}
 
 	@Override
-	public long getGroupId(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long getGroupId(int groupPosition) {
+		return groupPosition;
 	}
 
 	@Override
-	public View getGroupView(int arg0, boolean arg1, View arg2, ViewGroup arg3) {
-		// TODO Auto-generated method stub
-		View v = arg2;
-		if(v ==  null)
-			v = inflater.inflate(R.layout.expandable_category, null);
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView
+            , ViewGroup parent) {
+		View resultView = convertView;
+		if(resultView ==  null)
+			resultView = inflater.inflate(R.layout.expandable_category, null);
 		
-		TextView catText = (TextView)v.findViewById(R.id.textView_categoryName);
-		catText.setText(catList.get(arg0));
-		return v;
+		TextView catText = (TextView)resultView.findViewById(R.id.textView_categoryName);
+		catText.setText(catList.get(groupPosition));
+		return resultView;
 	}
 
 	@Override
 	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
-	public boolean isChildSelectable(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
 		return true;
 	}
 
